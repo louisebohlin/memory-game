9aabebc6591397344c04a7a072f159ea2e2c8faa
louisebohlin <37780677+louisebohlin@users.noreply.github.com>
2019-05-02 18:41:30 +0200

    Matching card works

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -33,16 +33,19 @@ const cards = [
     }
 ]
 
-const game = document.getElementById('game');
+let gameGrid = cards.concat(cards)
+gameGrid.sort(() => 0.5 - Math.random())
 
-let count = 0;
+const game = document.getElementById('game');
 
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
-let gameGrid = cards.concat(cards)
-gameGrid.sort(() => 0.5 - Math.random())
+let count = 0;
+let firstGuess = '';
+let secondGuess = '';
+let previousTarget = null;
 
 gameGrid.forEach(item => {
     const card = document.createElement('div')
@@ -52,17 +55,36 @@ gameGrid.forEach(item => {
     grid.appendChild(card)
 })
 
+const match = () => {
+  var selected = document.querySelectorAll('.selected')
+  selected.forEach(card => {
+    card.classList.add('match')
+  })
+}
+
 grid.addEventListener('click', function(event) {
 
     let clicked = event.target
 
-    if (clicked.nodeName === 'SECTION') {
+    if (clicked.nodeName === 'SECTION'|| clicked === previousTarget) {
         return
       }
 
     if (count < 2) {
       count++
-    clicked.classList.add('selected')
+      if (count === 1) {
+        firstGuess = clicked.dataset.name
+        clicked.classList.add('selected')
+      } else {
+        secondGuess = clicked.dataset.name
+        clicked.classList.add('selected')
+      }
+      if (firstGuess !== '' && secondGuess !== '') {
+        if (firstGuess === secondGuess) {
+          match()
+        }
+      }
+      previousTarget = clicked;
   }}
 )
 
